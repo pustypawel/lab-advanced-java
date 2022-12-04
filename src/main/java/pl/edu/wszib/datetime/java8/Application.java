6fe8e454252d4e360ccd0699e36a5a17d786b4f6
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-12-04 11:11:22 +0100

    datetime - after java 8

---
+++ src/main/java/pl/edu/wszib/datetime/java8/Application.java
@@ -0,0 +1,51 @@
+package pl.edu.wszib.datetime.java8;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+public class Application {
+    public static void main(String[] args) {
+        Clock clock = Clock.systemDefaultZone();
+
+        // Instant - reprezentuje konkretny "moment" (ilość sekund, która upłynęła od 1970)
+        Instant instant = Instant.now(clock);
+        // Period/Duration
+        System.out.println("Instant: " + instant);
+        Instant newPlus1s = instant.plusSeconds(1);
+        System.out.println("Instant: " + newPlus1s);
+
+        // LocalDate, LocalTime, LocalDateTime
+        LocalDate localDate = LocalDate.now(clock);
+        System.out.println("LocalDate: " + localDate);
+        LocalTime localTime = LocalTime.now(clock);
+        System.out.println("LocalTime: " + localTime);
+        LocalDateTime localDateTime = localDate.atTime(localTime);
+        System.out.println("LocalDateTime: " + localDateTime);
+        LocalDateTime localDateTime1 = LocalDateTime.now(clock);
+        // ISO 8601
+
+        // OffsetDateTime
+//        localDateTime.atOffset(ZoneId.systemDefault())
+        OffsetDateTime offsetDateTime = OffsetDateTime.now(clock);
+        System.out.println("OffsetDateTime: " + offsetDateTime);
+
+        // ZonedDateTime
+        ZonedDateTime zonedDateTime = offsetDateTime.toZonedDateTime();
+        ZonedDateTime zonedDateTime1 = ZonedDateTime.now(clock);
+        System.out.println("ZonedDateTime: " + zonedDateTime);
+        System.out.println("ZonedDateTime: " + zonedDateTime1);
+
+        // Period
+        Period period = Period.of(1, 1, 1);
+        System.out.println("Period: " + period);
+
+        // Duration
+        Duration duration = Duration.ofDays(1);
+        System.out.println("Duration: " + duration);
+        Duration duration1 = Duration.ofMinutes(1);
+        System.out.println("Duration: " + duration1);
+        Duration duration2 = Duration.ofSeconds(1);
+        System.out.println("Duration: " + duration2);
+    }
+}
